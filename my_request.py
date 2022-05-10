fd5d82c513fea2b4a9e2734b5cbdc0db8f5dbb6a
Serj Boris <priserv@tuta.io>
2022-05-10 19:54:47 +0300

    add my_request.py

---
+++ my_request.py
@@ -0,0 +1,46 @@
+import requests
+from requests.exceptions import HTTPError
+import urllib3
+
+
+def my_request(url: str, param: dict, cook={}, method='get',
+                print_resp=False, print_param=True) -> str:
+    if print_param:
+        print(*[n[0]+'='+n[1] for n in sorted(param.items())])
+    try:
+        response = (requests.get(url, params=param, cookies=cook)
+                    if method == 'get' else
+                    requests.post(url, data=param, cookies=cook))
+        if print_resp:
+            if method == 'get':
+                print(response.url)
+            print(response.text)
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        if 'SSLCertVerificationError' in str(err):
+            response = my_request_not_verify(url, param, cook, method)
+            return response.text
+        else:
+            print(f'Other error occurred: {err}')
+            exit(1)
+    else:
+        return response.text
+
+
+def my_request_not_verify(url: str, param: dict, cook: dict, method: str) -> str:
+    urllib3.disable_warnings()
+    try:
+        response = (requests.get(url, params=param, cookies=cook, verify=False)
+                    if method == 'get' else
+                    requests.post(url, data=param, cookies=cook, verify=False))
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+        exit(1)
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+        exit(1)
+    else:
+        return response
