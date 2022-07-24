1832c9df1e1c3ad85a92fcbe1ca57ff419d37b8c
Serj Boris <priserv@tuta.io>
2022-07-24 07:32:41 -0400

    sqli

---
+++ sqli.py
@@ -0,0 +1,11 @@
+from bs4 import BeautifulSoup
+import re
+import difflib
+from itertools import compress
+from typing import Callable
+import time
+from connect import my_request as req
+
+
+class Sqli:
+    pass
